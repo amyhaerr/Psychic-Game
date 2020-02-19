76670b060a8f34b77e1b9428fa3dd630b6258786
Sangriale <senora.pelos@gmail.com>
2020-02-19 09:40:52 -0600

    add HTML and some javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// An array of possible letter options
+var computerChoices = ["s", "n", "a", "p", "e"];
+
+// Variable assigned to hold 5 letters to be guessed
+var lettertoGuess = null;
+
+
+// Create variable to hold Wins, Losses, Guesses Left, and Letters Guessed
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
