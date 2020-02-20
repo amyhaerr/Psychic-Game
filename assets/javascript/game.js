d73b786379fb0c4b9a64bfea9b577a0073a21095
Sangriale <senora.pelos@gmail.com>
2020-02-20 10:39:21 -0600

    add more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,58 @@
 // An array of possible letter options
 var computerChoices = ["s", "n", "a", "p", "e"];
 
+
+// Array holds letters guessed
+var lettersGuessed = [];
+
 // Variable assigned to hold 5 letters to be guessed
-var lettertoGuess = null;
+var letterToGuess = null;
 
 
-// Create variable to hold Wins, Losses, Guesses Left, and Letters Guessed
+// Create variables to hold Wins, Losses, Guesses Left, and Letters Guessed
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var lettersGuessed = [];
+var numGuesses = 9;
+
+
+
+// Function runs whenever the player presses a key
+document.onkeyup = function(event) {
+
+    // Determines key pressed
+    var userGuess = event.key;
+
+    // Randomly chooses choice from options in the array
+    var computerChoices = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if(options.indexOf(userGuess) >-1) {
+        if(userGuess===computerGuess) {
+            wins++;
+            numGuesses=9;
+            lettersGuessed=[];
+        }
+   
+        if(userGuess !=computerGuess) {
+            numGuesses--;
+            lettersGuessed.push(userGuess);
+        }
+        
+        if(numGuesses ===0) {
+            numGuesses=9;
+            losses++;
+            lettersGuessed = [];
+        }
+   
+    };
+
+
+
+
+
+
+    // userChoiceText.textContent = "You chose: " + userGuess;
+    
+    // winsText.textContent = "wins: " + wins;
+    // lossesText.textContent = "losses: " + losses;
+}
