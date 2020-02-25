6b79377b8e5e59692cdc593b410dffb4345c2474
Sangriale <senora.pelos@gmail.com>
2020-02-25 12:14:57 -0600

    fix JavaScript if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,73 @@
-
+// variables to store information for the game
 var computerChoices = ["s", "n", "a", "p", "e"];
 var userGuess = "";
-var numGuesses = 6;
+var numGuesses = 9;
 var lettersGuessed = [];
 var wins = 0;
 var losses = 0;
 
 
 
-
-
+// variable to determine computer choice from provided array
 var computerGuess = computerChoices[Math.floor(Math.random()
     * computerChoices.length)];
 
+// update guesses left
+function countNumGuesses() {
+    document.querySelector("#number-text").innerHTML = numGuesses;
+}
+
+// takes user guess and join string and visible on screen
+
+function farUserGuesses() {
+    document.querySelector("#letters-text").innerHTML = lettersGuessed.join(", ");
+}
+
+countNumGuesses();
 
 function resetGame() {
-    numGuesses = 6;
+    numGuesses = 9;
     lettersGuessed = [];
-    computerGuess = computerChoices[Math.floor(Math.random()
+
+    var computerGuess = computerChoices[Math.floor(Math.random()
         * computerChoices.length)];
 }
 
 document.onkeyup = function (event) {
 
-    var letters = event.key.toLowerCase();
+
+    var userGuess = event.key.toLowerCase();
+
+    farUserGuesses();
+    countNumGuesses();
+
+    if (userGuess !== computerGuess) {
+        numGuesses--;
+        lettersGuessed.push(userGuess);
+        // document.querySelector("#number-text").innerHTML=numGuesses;
+
+    }
+     if (userGuess === computerGuess) {
+        wins++;
+        numGuesses=9
+        // document.querySelector("#wins-text").innerHTML=wins;
+        resetGame()
+
+    } 
+     if (numGuesses === 0) {
+        losses++;
+        lettersGuessed = [];
+        numGuesses = 9;
+        // document.querySelector("#losses-text").innerHTML = losses;
+        resetGame();
+    }
 
 
-    var userGuess = event.key;
 
 
-        if (userGuess !== computerGuess) {
-            numGuesses--;
-            lettersGuessed.push(userGuess);
-        }
-           else if (userGuess === computerGuess) {
-                wins++;
-                resetGame()
+    document.getElementById("number-text").textContent = "Guesses Left:" + numGuesses;
+    document.getElementById("letters-text").textContent = "Letters you have Guessed:" + lettersGuessed;
+    document.getElementById("wins-text").textContent = "Wins: " + wins;
+    document.getElementById("losses-text").textContent = "Losses: " + losses;
 
-            } else if (numGuesses === 0) {
-                losses++;
-                resetGame();
-            }
-
-        
-
-
-        document.getElementById("number-text").textContent = "Guesses Left:" + numGuesses;
-        document.getElementById("letters-text").textContent = "Letters you have Guessed:" + lettersGuessed;
-        document.getElementById("wins-text").textContent = "Wins: " + wins;
-        document.getElementById("losses-text").textContent = "Losses: " + losses;
-
-    };
-// }
+};
